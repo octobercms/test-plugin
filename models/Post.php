fa3142ffd1e6ec7a57c3eaf6a9f6736531278a3c
Luke Towers <github@luketowers.ca>
2016-08-03 13:23:10 -0600

    Test for Provides event to extend relation's configuration octobercms/october#2245
    This is the test for octobercms/october#2245 that demonstrates the need for an event to modify the relationController's configuration.

--- models/Post.php
+++ models/Post.php
@@ -44,4 +44,17 @@ class Post extends Model
         'status' => ['October\Test\Models\Attribute', 'conditions' => "type = 'general.status'"],
     ];
 
+    // Demonstrates inability to disable a relationController through filterFields
+    public function filterFields($fields, $context = null) {
+        // If the post is 'active', then apply rules
+        if ($context = 'update' && $this->status && $this->status->code === 'active') {
+            // Disable the name field from being modified as the post is active
+            $fields->name->disabled = true;
+            
+            // Attempt to disable the comments 'field' as the post is active
+            // NOTE: This does not work as the 'field' is a partial calling `$this->relationRender();`
+            //       so disabling it doesn't actually change the readOnly property of the relationController
+            $fields->comments->disabled = true;
+        }
+    }
 }
