fbf69f2f8ee4d2fbb71f287575e510f1689da1c7
Meysam Mahfouzi <maysam.mahfouzi@gmail.com>
2017-03-25 14:09:12 +0430

    Add two report widgets

---
+++ reportwidgets/SemiCircle.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace October\Test\ReportWidgets;
+
+use Backend\Classes\ReportWidgetBase;
+
+class SemiCircle extends ReportWidgetBase
+{
+    /**
+     * @var string A unique alias to identify this widget.
+     */
+    protected $defaultAlias = 'semiCircle';
+
+    public function render()
+    {
+        $this->vars['id'] = rand();
+        return $this->makePartial('widget');
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'title' => [
+                'title'             => 'Title',
+                'default'           => 'Visitor Traffic',
+                'type'              => 'string',
+                'validationPattern' => '^.+$',
+                'validationMessage' => 'The Widget Title is required.'
+            ],
+            'legend' => [
+                'title'             => 'Legend',
+                'default'           => 'Visitors',
+                'type'              => 'string',
+                'validationPattern' => '^.+$',
+                'validationMessage' => 'The chart legend is required.'
+            ],
+            'days' => [
+                'title'             => 'Number of days to display data for',
+                'default'           => '16',
+                'type'              => 'string',
+                'validationPattern' => '^[0-9]+$'
+            ]
+        ];
+    }
+
+    public function loadAssets()
+    {
+        $this->addJs('js/highcharts.js');
+    }
+}
