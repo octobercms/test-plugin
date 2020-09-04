ddc600337385bae56f3e7bccf07996b42dc71ac5
Luke Towers <github@luketowers.ca>
2020-09-04 14:31:03 -0600

    Add example of how to disable a scope based on the value of another scope. Requires https://github.com/octobercms/october/commit/91e4e9692971496091e86668a1a4be70aeae62ab

--- controllers/Users.php
+++ controllers/Users.php
@@ -26,4 +26,28 @@ class Users extends Controller
 
         BackendMenu::setContext('October.Test', 'test', 'users');
     }
+
+    public function listFilterExtendScopes($filter)
+    {
+        // Demonstrate how to disable a scope based on the value of another scope
+        if (input('scopeName') === 'disable_roles') {
+            $disableRoles = input('value');
+            if ($disableRoles === 'false') {
+                $disableRoles = false;
+            }
+        } else {
+            $disableRoles = $filter->getScopeValue('disable_roles');
+        }
+
+        if ($disableRoles) {
+            $filter->removeScope('roles');
+        }
+
+        $filter->bindEvent('filter.update', function ($params) use ($filter) {
+            $filter->prepareVars();
+            return [
+                '#' . $filter->getId() => $filter->makePartial('filter_scopes', ),
+            ];
+        });
+    }
 }
