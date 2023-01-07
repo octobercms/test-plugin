4a19bad71e2deece6e74540d34e9ce063abba480
October CMS <hello@octobercms.com>
2023-01-07 11:04:18 +1100

    Testing dropdown in list

---
+++ models/product/_column_dropdown.php
@@ -0,0 +1,10 @@
+<div class="btn-group">
+    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Dropdown
+    </button>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Menu item</a></li>
+        <li><a class="dropdown-item" href="#">Menu item</a></li>
+        <li><a class="dropdown-item" href="#">Menu item</a></li>
+    </ul>
+</div>
