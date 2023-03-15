b43367799d0991d1b44611365ce75a26cc644709
October CMS <hello@octobercms.com>
2023-03-16 10:28:45 +1100

    Scaffold browser test

--- tests/browser/test-this/ProductsTest.php
+++ tests/browser/test-this/ProductsTest.php
@@ -64,4 +64,42 @@ class ProductsTest extends BrowserTestCase
                 ->click('a.flash-close');
         });
     }
+
+    /**
+     * testProductCategoriesCreate
+     */
+    public function testProductCategoriesCreate()
+    {
+        $this->browse(function($browser) {
+            $browser
+                ->visit('/admin/october/test/productcategories')
+                ->assertTitleContains('Manage Product Categories |')
+            ;
+
+            $browser
+                ->clickLink('New Product Category')
+                ->waitForLocation('/admin/october/test/productcategories/create')
+                ->waitForEvent('page:load', 'document')
+            ;
+
+            $browser
+                ->type('ProductCategory[name]', 'Dusk Test')
+                ->click('.form-buttons [data-request=onSave]')
+                ->waitForTextIn('.oc-flash-message.success', 'Product Category Created')
+                ->click('a.flash-close')
+            ;
+
+            $browser
+                ->ajaxRequest('.form-buttons [data-request=onDelete]')
+                ->waitForTextIn('.modal-body > p', 'Delete record?')
+                ->press('OK')
+            ;
+
+            $browser
+                ->waitForLocation('/admin/october/test/productcategories')
+                ->waitForTextIn('.oc-flash-message.success', 'Product Category Deleted')
+                ->click('a.flash-close')
+            ;
+        });
+    }
 }
