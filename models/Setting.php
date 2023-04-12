1fc4a0525a39434c74b4cc75699d523a2827a65e
October CMS <hello@octobercms.com>
2023-04-12 11:18:48 +1000

    Adds settings backwards compatibility test

--- models/Setting.php
+++ models/Setting.php
@@ -1,16 +1,19 @@
 <?php namespace October\Test\Models;
 
 use System\Models\SettingModel;
+use October\Rain\Database\Model;
 
 /**
  * Setting model
  */
 class Setting extends SettingModel
+// class Setting extends Model
 {
     // use \October\Rain\Database\Traits\Multisite;
     use \October\Rain\Database\Traits\Validation;
 
     public $implement = [
+        // \System\Behaviors\SettingsModel::class,
         '@'.\RainLab\Translate\Behaviors\TranslatableModel::class
     ];
 
