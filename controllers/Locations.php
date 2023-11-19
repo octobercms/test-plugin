9135c005e58b99ea8c5ac6b78abce8c8be95c771
October CMS <hello@octobercms.com>
2023-11-20 10:47:43 +1100

    Adds example of manually eager loading attachment relations

--- controllers/Locations.php
+++ controllers/Locations.php
@@ -24,4 +24,41 @@ class Locations extends Controller
 
         BackendMenu::setContext('October.Test', 'test', 'locations');
     }
+
+    /**
+     * formExtendModel eager loads the attachment relations
+     * @todo in v4 this can be replaced with a single method call i.e loadAttachments()
+     */
+    public function formExtendModel($model)
+    {
+        if ($model->exists) {
+            $foundFiles = \System\Models\File::where('attachment_id', $model->getKey())
+                ->where('attachment_type', get_class($model))
+                ->get()
+                ->groupBy('field')
+                ->all();
+
+            foreach ($model->getRelationDefinitions() as $type => $relations) {
+                if ($type !== 'attachOne' && $type !== 'attachMany') {
+                    continue;
+                }
+
+                foreach ($relations as $relation => $definition) {
+                    if (isset($foundFiles[$relation])) {
+                        $files = $foundFiles[$relation];
+                        if ($type === 'attachOne') {
+                            $model->setRelation($relation, $files->first());
+                        }
+                        else {
+                            $model->setRelation($relation, $files);
+                        }
+                    }
+                    else {
+                        $model->setRelation($relation, null);
+                    }
+                }
+
+            }
+        }
+    }
 }
