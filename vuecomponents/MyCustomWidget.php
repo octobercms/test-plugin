f3ea2b5a31cdb1203950922646ee2b5d18e0c2e9
Samuel Georges <sam@daftspunk.com>
2025-06-08 11:31:14 +1000

    Updating dashboard spec

--- vuecomponents/MyCustomWidget.php
+++ vuecomponents/MyCustomWidget.php
@@ -1,20 +1,13 @@
 <?php namespace October\Test\VueComponents;
 
-use Backend\Classes\VueReportWidgetBase;
+use Dashboard\Classes\VueReportWidgetBase;
+use Dashboard\Classes\ReportFetchData;
 use Carbon\Carbon;
 
 class MyCustomWidget extends VueReportWidgetBase
 {
-    public function getData(
-        array $widgetConfig,
-        ?Carbon $dateStart,
-        ?Carbon $dateEnd,
-        ?int $startTimestamp,
-        ?Carbon $compareDateStart,
-        ?Carbon $compareDateEnd,
-        ?string $aggregationInterval,
-        array $extraData
-    ): mixed {
+    public function getData(ReportFetchData $data): mixed
+    {
         return [
             'current_time' => Carbon::now()->format('Y-m-d H:i:s')
         ];
