71fc78abc2d706811b0e8582231c41a626a0adb9
danielbidala <danielbidala@gmail.com>
2025-10-21 17:14:02 +0200

    Props doesn't update

--- reportwidgets/clearcache/partials/_clearcache.php
+++ reportwidgets/clearcache/partials/_clearcache.php
@@ -3,6 +3,8 @@
 
     <?php if (!isset($error)): ?>
         <p>This is the default partial content.</p>
+        <p>Title: <?= $this->property('title') ?></p>
+        <p>Days: <?= $this->property('days') ?></p>
         <button data-request="<?= $this->getEventHandler('onClearCache') ?>" class="btn btn-outline-warning">
             <span class="fa icon-trash"></span> Clear Cache</button>
     <?php else: ?>
