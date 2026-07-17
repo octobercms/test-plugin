18f74f5ea17fc5f7b51aaa83f0457274411961e4
Samuel Georges <sam@daftspunk.com>
2026-07-17 18:36:31 +1000

    testing sidebar with lots of tabs

--- reportwidgets/clearcache/partials/_clearcache.php
+++ reportwidgets/clearcache/partials/_clearcache.php
@@ -7,8 +7,10 @@
         <p>Days (prop): <?= $this->property('days') ?></p>
         <p>User (prop): <?= $this->property('user') ?></p>
         <p>Users (prop): <?= implode(', ', (array) $this->property('users')) ?></p>
+
         <button data-request="<?= $this->getEventHandler('onClearCache') ?>" class="btn btn-outline-warning">
-            <span class="icon-delete"></span> Clear Cache</button>
+            <span class="icon-delete"></span> Clear Cache
+        </button>
     <?php else: ?>
         <p class="flash-message static warning"><?= e($error) ?></p>
     <?php endif ?>
